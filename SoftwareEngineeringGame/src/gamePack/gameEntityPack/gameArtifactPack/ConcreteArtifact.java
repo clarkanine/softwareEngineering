fb9c3f64e969d09093febe56813c031a2cf283b1
Aaron Clark <aclark56@eagles.ewu.edu>
2016-04-29 14:09:58 -0700

    Unit test for GameCharacter

---
+++ SoftwareEngineeringGame/src/gamePack/gameEntityPack/gameArtifactPack/ConcreteArtifact.java
@@ -0,0 +1,69 @@
+package gamePack.gameEntityPack.gameArtifactPack;
+
+public class ConcreteArtifact implements GameArtifact
+{
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getView()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setView(String view)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateView()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void spawn()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void kill()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void load()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
